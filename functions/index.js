37a5d293349aa7fad462d1991ca98e5e6bfb38ab
yanco3a <yanco3a@gmail.com>
2019-05-03 00:17:36 +0100

    Add script to Travis to install firebase functions and other dependencies. - changed css to minified version to speed page load - this will also change paths from the current:   /index.html -> /index   /about.html -> /about   /contact.html -> /contact   /home.html -> /home - to test locally from firebase CLI use: firebase serve - change `index.html` line: 61 from:   `<em>Community for Black Female Developers- TEST</em>`  to   `<em>Community for Black Female Developers</em>`
    Solo: @yanco3a

--- functions/index.js
+++ functions/index.js
@@ -2,4 +2,35 @@ const functions = require('firebase-functions');
 const express = require('express')
 const app = express()
 
+const path = require('path'); //uncomment to use .html
+// const cons = require('consolidate'); //uncomment to use .html
+
+// public folder where public assets are stored
+app.use(express.static(path.join('../', 'app')))
+
+// to use index.html, about.html, contact.html, home.html
+// uncomment to use .html
+// view engine setup
+// app.engine('html', cons.swig)
+// app.set('views', path.join(__dirname, 'app'));
+// app.set('view engine', 'html');
+
+// comment out below until *end* to use the ".html" extension example above
+app.get('/', function (req, res) { // to use /index -> index.html
+    res.sendFile('index.html', { root: '../'+ 'app' })
+});
+
+app.get('/about', function (req, res) { // to use /about -> about.html
+    res.sendFile('about.html', { root: '../'+ 'app' })
+});
+
+app.get('/contact', function (req, res) { // to use /contact -> home.html
+    res.sendFile('contact.html', { root: '../'+ 'app' })
+});
+
+app.get('/home', function (req, res) { // to use /home -> home.html
+    res.sendFile('home.html', { root: '../'+ 'app' })
+});
+// *end*
+
 exports.cbf = functions.https.onRequest(app);
